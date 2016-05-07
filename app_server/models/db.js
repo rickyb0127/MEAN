dac1fa5b674eb5c70ae21b42e39e7ea22ff42a0a
eric bartsch <bartsched@gmail.com>
2016-05-07 16:02:28 -0400

    connected mongoose and mongodb

---
+++ app_server/models/db.js
@@ -0,0 +1,65 @@
+var mongoose = require('mongoose');
+var gracefulShutdown;
+var dbURI = 'mongodb://localhost/Loc8r';
+var dbURIlog = 'mongodb://localhost/Loc8rLog';
+if (process.env.NODE_ENV === 'production') {
+  dbURI = process.env.MONGOLAB_URI;
+}
+var logDB = mongoose.createConnection(dbURIlog);
+var readLine = require('readline');
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
+
+logDB.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURIlog);
+});
+logDB.close(function () {
+  console.log('Mongoose log disconnected');
+});
+
+gracefulShutdown = function (msg, callback) {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+
+process.once('SIGUSR2', function () {
+    gracefulShutdown('nodemon restart', function () {
+      process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.on('SIGINT', function () {
+  gracefulShutdown('app termination', function () {
+    process.exit(0);
+  });
+});
+
+process.on('SIGTERM', function () {
+  gracefulShutdown('Heroku app shutdown', function () {
+    process.exit(0);
+  });
+});
+
+if (process.platform === 'win32') {
+  var rl = readLine.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on("SIGINT", function () {
+    process.emit("SIGINT");
+  });
+}
+
+require('./locations');
